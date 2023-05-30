fd254f97e7287941cc1466b7b08a1ffee38b5eb1
allan <allankipchumba76@gmail.com>
2023-05-30 20:16:54 +0300

    completed project structure

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover and share</h1>
+    </section>
+  );
+}
