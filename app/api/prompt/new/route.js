98eed237e2a241050c79064e39e93403a52e17cd
allan <allankipchumba76@gmail.com>
2023-06-02 10:51:22 +0300

    created create Prompt api and client code and prompt schema

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,29 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const POST = async (req, res) => {
+  //access data  from client
+  const { userId, prompt, tag } = await req.json();
+
+  //save data to DB
+  try {
+    /*
+    we connect to the db everytime because it is a lambda function,
+    it isgoing to 'die' once it does its job. Every time it is called,
+     it needs to connect to the db and then 'go in peace'
+    */
+    await connectToDB();
+
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (error) {
+    return new Response("Failed to creste a new prompt", { status: 500 });
+  }
+};
--- app/create-prompt/page.js
+++ app/create-prompt/page.js
@@ -1,5 +1,51 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+//useSession tells which user is currently logged in
+import { useSession } from "next-auth/react";
+import Form from "@components/Form";
+import { useRouter } from "next/navigation";
 
 export default function CreatePrompt() {
-  return <div>CreatePrompt</div>;
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 }
